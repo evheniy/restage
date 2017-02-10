db0961cb2c449e26a0b640b188eac4a95bdc5903
Evheniy Bystrov <evheniy.bystrov@just-eat.com>
2017-02-10 12:45:18 +0000

    new app and tests

---
+++ src/app/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* export default () => {
+    ReactDOM.render(
+        <h1>Test</h1>,
+        document.getElementById('root')
+    );
+} */
+
+export default () => <div><h1>Test</h1></div>;
